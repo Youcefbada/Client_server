1e02c23244ea062e49086c981d888db2c19a88fd
youcef <youcefbada04@gmail.com>
2024-07-07 16:50:20 +0100

    Server

---
+++ src/api/thum/routes/thum.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * thum router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::thum.thum');
---
+++ src/api/thum/services/thum.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * thum service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::thum.thum');
